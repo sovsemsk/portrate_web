c89b05a27c724fb96142aa32574c1f79c019846e
Eugene Zarechkin <sovsemsk@gmail.com>
2023-09-25 11:33:03 +0300

    ...

--- po_web/settings.py
+++ po_web/settings.py
@@ -81,17 +81,17 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator' },
-    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator' },
-    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator' },
-    { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator' }
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # @TODO: Убрать хардкод
-DEBUG = True
+# DEBUG = True
 
-# 
+#
 ALLOWED_HOSTS = ['geo.portrate.io', '91.201.53.6', '127.0.0.1']
 INTERNAL_IPS = ['127.0.0.1']
 
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
