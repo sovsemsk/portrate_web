a643d3e3d428d9fef3f7421aa6c2524b7283f831
Eugene Zarechkin <sovsemsk@gmail.com>
2023-09-25 14:17:36 +0300

    Fixes

--- po_web/settings.py
+++ po_web/settings.py
@@ -89,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # @TODO: Убрать хардкод
-DEBUG = True
+# DEBUG = True
 
 #
 ALLOWED_HOSTS = ['geo.portrate.io', '91.201.53.6', '127.0.0.1']
