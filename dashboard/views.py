f0d7c92174d2e0195cea9ece91fc0d594fbf484b
Eugene Zarechkin <sovsemsk@gmail.com>
2023-10-29 23:09:07 +0300

    Оповещения

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.decorators.http import require_http_methods
-from resources.models import Company
+from resources.models import Company, Notification
 
 
 @login_required
@@ -24,7 +24,9 @@ def companies(request):
 @require_http_methods(['GET'])
 def notifications(request):
     companies = Company.objects.filter(users__in=(request.user,)).all()
+    notifications = Notification.objects.filter(company__in=companies).all()
     return render(request, 'dashboard/notifications.html', {
         'nav': 'notifications',
-        'companies': companies
+        'companies': companies,
+        'notifications': notifications
     })
