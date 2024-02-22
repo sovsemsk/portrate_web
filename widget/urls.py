dbaa404a27397249da42d11df939b30a4eba9a8d
Eugene Zarechkin <sovsemsk@gmail.com>
2024-02-22 09:57:57 +0500

    Iframe виджет

--- widget/urls.py
+++ widget/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:company_api_secret>/javascript.js", views.javascript, name="widget_javascript"),
     path("<str:company_api_secret>/rate", views.rate, name="widget_rate"),
 ]
 
