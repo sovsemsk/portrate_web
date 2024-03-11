da625e971dff8414fff71679897aa1b0bb2fe923
Eugene Zarechkin <sovsemsk@gmail.com>
2024-03-11 14:22:44 +0500

    Фикс шрифтов виджета отзывов

--- widget/templates/widget/rate.js
+++ widget/templates/widget/rate.js
@@ -3,7 +3,4 @@ const pioRateBody = document.querySelector("body");
 const pioRateNode = new DOMParser().parseFromString(pioRateTemplate, "text/html").body.firstElementChild;
 const pioRateWidgetScriptTag = document.querySelector("#PIO_RATE_WIDGET_SCRIPT");
 pioRateBody.appendChild(pioRateNode);
-
-//const starGray = document.getElementById("STAR_GRAY");
-//const starYellow = document.getElementById("STAR_YELLOW");
-//starGray.onload = () => starYellow.onload = () => { alert("Hello") };
+// starGray.onload = () => starYellow.onload = () => {};
--- widget/templates/widget/reviews.js
+++ widget/templates/widget/reviews.js
@@ -1,4 +1,4 @@
-const pioReviewsInsertAfter = (referenceNode, newNode) => {referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);}
+const pioReviewsInsertAfter = (referenceNode, newNode) => {referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)}
 const pioReviewsTemplate = `{% include "widget/_reviews.html" %}`;
 const pioReviewsBody = document.querySelector("body");
 const pioReviewsNode = new DOMParser().parseFromString(pioReviewsTemplate, "text/html").body.firstElementChild;
@@ -7,18 +7,8 @@ pioReviewsInsertAfter(pioReviewsWidgetScriptTag, pioReviewsNode);
 const pioReviewsWidgetContainer = document.querySelector("#PIO_REVIEWS_WIDGET_CONTAINER");
 const pioReviewsWidgetContainerWidth = pioReviewsWidgetContainer.offsetWidth;
 const pioReviewsWidgetCards = document.querySelectorAll(".PIO_REVIEWS_WIDGET_CARD");
-if (pioReviewsWidgetContainerWidth < 480){
-    pioReviewsWidgetCards.forEach(element => {element.classList.add('PIO_REVIEWS_WIDGET_CARD_1')})
-}
-if (pioReviewsWidgetContainerWidth > 481 && pioReviewsWidgetContainerWidth < 768){
-    pioReviewsWidgetCards.forEach(element => {element.classList.add('PIO_REVIEWS_WIDGET_CARD_2')})
-}
-if (pioReviewsWidgetContainerWidth > 769 && pioReviewsWidgetContainerWidth < 1024){
-    pioReviewsWidgetCards.forEach(element => {element.classList.add('PIO_REVIEWS_WIDGET_CARD_3')})
-}
-if (pioReviewsWidgetContainerWidth > 1025 && pioReviewsWidgetContainerWidth < 1200){
-    pioReviewsWidgetCards.forEach(element => {element.classList.add('PIO_REVIEWS_WIDGET_CARD_4')})
-}
-if (pioReviewsWidgetContainerWidth > 1200){
-    pioReviewsWidgetCards.forEach(element => {element.classList.add('PIO_REVIEWS_WIDGET_CARD_5')})
-}
+if(pioReviewsWidgetContainerWidth < 480){pioReviewsWidgetCards.forEach(element => {element.classList.add("PIO_REVIEWS_WIDGET_CARD_1")})}
+if(pioReviewsWidgetContainerWidth > 481 && pioReviewsWidgetContainerWidth < 768){pioReviewsWidgetCards.forEach(element => {element.classList.add("PIO_REVIEWS_WIDGET_CARD_2")})}
+if(pioReviewsWidgetContainerWidth > 769 && pioReviewsWidgetContainerWidth < 1024){pioReviewsWidgetCards.forEach(element => {element.classList.add("PIO_REVIEWS_WIDGET_CARD_3")})}
+if(pioReviewsWidgetContainerWidth > 1025 && pioReviewsWidgetContainerWidth < 1200){pioReviewsWidgetCards.forEach(element => {element.classList.add("PIO_REVIEWS_WIDGET_CARD_4")})}
+if(pioReviewsWidgetContainerWidth > 1200){pioReviewsWidgetCards.forEach(element => {element.classList.add("PIO_REVIEWS_WIDGET_CARD_5")})}
