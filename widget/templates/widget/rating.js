5dd7945546d4639cae4463467b6c67cf4346f87c
Eugene Zarechkin <sovsemsk@gmail.com>
2024-04-05 11:41:13 +0500

    Виджет с лейаутом слайдер

--- widget/templates/widget/rating.js
+++ widget/templates/widget/rating.js
@@ -1,3 +1,5 @@
 __c9a413__ = document.querySelector("body");
+// Script
 __45e615__ = new DOMParser().parseFromString(`{% include "widget/_rating.html" %}`, "text/html").body.firstElementChild;
+// Add node
 __c9a413__.appendChild(__45e615__);
