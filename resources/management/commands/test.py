15a9b77f7ef1a2d8e4ffe429c34dd063f9b5ca7c
Eugene Zarechkin <sovsemsk@gmail.com>
2024-08-02 11:55:27 +0500

    Тест парсера Яндекс

--- resources/management/commands/test.py
+++ resources/management/commands/test.py
@@ -11,13 +11,10 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		parsers_chain = []
-		company = Company.objects.get(pk=49)
-		r = Review.objects.get(pk=1)
+		company = Company.objects.get(pk=22)
 
-		print(r.notification_template)
-
-		# if company.parser_link_yandex:
-		# 	parsers_chain.append(parse_yandex_task.s(company_id=company.id))
+		if company.parser_link_yandex:
+			parsers_chain.append(parse_yandex_task.s(company_id=company.id))
 
 		# if company.parser_link_gis:
 		# 	parsers_chain.append(parse_gis_task.s(company_id=company.id))
@@ -25,4 +22,4 @@ class Command(BaseCommand):
 		# if company.parser_link_google:
 		# 	parsers_chain.append(parse_google_task.s(company_id=company.id))
 
-		# chain(*parsers_chain).apply_async()
+		chain(*parsers_chain).apply_async()
