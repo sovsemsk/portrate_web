6c777ba50f7ef3a2f6fdf5084b860e9b0ded1029
Evgeny Zarechkin <evgeny.zarechkin@simbirsoft.com>
2024-11-24 21:22:28 +0300

    Фиксы для парсеров

--- parsers/prodoctorov.py
+++ parsers/prodoctorov.py
@@ -97,14 +97,14 @@ class ReviewsPage():
 
             if len(text_els) == 1:
                 text = text_els[0].get_attribute("textContent")
-            elif len(text_els) > 1:
-                text = f"{
-                text_els[0].get_attribute('textContent') if text_els[0] in text_els else ''
-                }. {
-                text_els[1].get_attribute('textContent') if text_els[1] in text_els else ''
-                }. {
-                text_els[2].get_attribute('textContent') if text_els[2] in text_els  else ''
-                }"
+            # elif len(text_els) > 1:
+            #     text = f"{
+            #     text_els[0].get_attribute('textContent') if text_els[0] in text_els else ''
+            #     }. {
+            #     text_els[1].get_attribute('textContent') if text_els[1] in text_els else ''
+            #     }. {
+            #     text_els[2].get_attribute('textContent') if text_els[2] in text_els  else ''
+            #     }"
             else:
                 text = None
 
